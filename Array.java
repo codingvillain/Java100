a3ff4ecb4ff7954b83776e71a9122f17678c1124
이혜원 <dingcoding486@gmail.com>
2022-06-30 05:17:21 +0900

    2022.06.30 배열

--- Array.java
+++ Array.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Array {
 
     public static void main(String[] args){
@@ -160,28 +163,199 @@ public class Array {
         /*
         7. 배열의 값들을 for문과 같은 반복문을 사용하지 않고 한꺼번에 출력하는 코드를 작성하시오.
             이 문제는 반복문이 아닌 메서드로 배열의 요소 값들을 출력하는 방법에 대해서 아는지를 묻는 문제이다.
-
+/
+        Arrays.toString() 메서드
+        반복문을 사용하지 않고 배열의 값을 출력하고자 한다면 Arrays 클래스의 toString() 메서드를 사용하면 된다.
+        toString() 인자로는 배열명을 넣어주면 된다.
+        사용을 위해서는 기본적으로 java.util.Arrays 임포트 선언이 되어져 있어야 한다. -> 세미콜론 입력 필수!
         * */
+
+        //import java.util.Arrays;
+        // 배열 선언
+        int[] ar7_1 = {238, 483, 239, 442, 487, 239, 873};
+
+        // toString() 메서드를 사용하여 값 출력
+        System.out.println(Arrays.toString(ar7_1));
         //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
         /*
-        2.
+        8. 다음 중 다중 배열의 선언으로 맞는 것을 골라보시오.
+           이 문제는 배열 선언을 여러 개 해야하는 경우 한중에 모두 선언하고 만들 수 있는지를 묻는 문제이다.
+
+           [1]
+           int[] a, b, c;
+
+           [2]
+           int d[], e[], f[];
+
+           [3]
+           int g[], h, i;  // g만 배열 선언이 되고 h와 i는 정수형 인자로 만들어진다.
         * */
+
         //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
         /*
-        2.
+        9. 배열을 복사하는 메서드를 말해보고 관련된 예제 코드를 구현해보시오.
+           이 문제는 배열 복사시 사용하는 메서드와 사용법에 대해서 알고 있는지를 묻는 문제이다.
+           [결과물]
+           [1,2,3,4,5]
+           [1,2,3,4,3,4,5,8,9,10]
+
+           arraycopy() 메서드 사용 -> 사용법 -> System.arraycopy(원본배열명, ???, 복사배열명, ???, 길이); // 원본배열명 : 복사할 배열의 시작점 , 복사배열명 : 덮어쓰기될 배열에서 시작점.
         * */
+        int[] ar9_1 = {1,2,3,4,5};
+        int [] ar9_2 = {1,2,3,4,5,6,7,8,9,10};
+
+        // 배열 복사하기 -> System.arraycopy();
+        System.arraycopy(ar9_1,2, ar9_2, 4, 3); // [1,2,3,4,3,4,5,8,9,10]
+        System.out.println(Arrays.toString(ar9_1));
+        System.out.println(Arrays.toString(ar9_2));
+
         //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
         /*
-        2.
+        10. 1,2차원 배열의 생성 및 출력을 다양한 예제로 구현해보시오.
+            이 문제는 자바 배열에 관해서 2차원 배열 위주로 생성 및 출력을 다양하게 해보는 종합문제입니다.
+
         * */
+            // [1] String 1차원 배열 -> 3개의 문자열을 담을 수 있는 배열 생성하기
+            // String 타입의 참조 변수 3개를 저장하기 위한 공간 생성 -> 이때, 참조형 변수 각 요소는 기본값 -> null로 초기화
+
+            String[] province = new String[3];
+            System.out.println(province[0]); // null
+            System.out.println(province[1]); // null
+            System.out.println(province[2]); // null
+
+            // [2] String 배열 초기화 -> 직접입력
+            province[0] = "강원도";
+            province[1] = "전라도";
+            province[2] = "경상도";
+
+            // [3] String 배열 초기화 -> 배열 선언 및 생성시 {}로 동시에 초기화
+            String[] nation = new String[]{"한국","미국","중국"};
+
+            String[] flower = {"무궁화","해바라기","개나리"};
+
+            // [4] 반복문을 사용하여 배열 값 모두 출력
+            for (int i = 0; i < flower.length; i++){
+                System.out.println(flower[i] + " ");
+            };
+            System.out.println();
+
+            //[5] 향상된 for문으로 배열 요소 출력
+            for (String aaa:flower)
+                System.out.println(aaa);
+
+            // [6] 2차원 배열 만들기 -> 2x3 Shape
+            // 아시아 -> 동북아시아 (한국, 중국, 일본), 동남아시아 (태국, 베트남, 필리핀)
+            String[][] asia_nations = {
+                    {"한국","중국","일본"},
+                    {"태국","베트남","필리핀"}
+            };
+
+        System.out.println(asia_nations[0]); // 1차원 배열의 주소를 참조하는 값이 출력
+
+        System.out.println(asia_nations[0][1]); // 중국
+        System.out.println(asia_nations[1][0]); // 태국
+
+        System.out.println(asia_nations.length); // 2
+        System.out.println(asia_nations[0].length); // 3
+        System.out.println(asia_nations[1].length); // 3
+
+        // [7] : 반복문을 사용한 String 2차원 배열 값 출력
+        for (int i = 0; i< asia_nations.length; i++){
+            for (int j = 0;j< asia_nations[i].length; j++){
+                System.out.print(asia_nations[i][j] + " ");
+            };
+                 System.out.println();
+        };
+
+        // [8] : charAt() 메서드 -> 해당 인덱스에 있는 값을 반환 -> 단어를 char 배열에 한글자씩 저장할 수 있음.
+        String[] atrAr = {"hong", "kim", "park"};
+        System.out.println(atrAr[0].charAt(2)); // n
+
+        // [9] : 반복문에서 charAt() 메서드 사용
+        // [9-1] 잘못된 예
+        for (int i = 0; i< atrAr.length; i++)
+            System.out.println(atrAr[0].charAt(i)); // hir -> 이중 반복문을 사용해야 한다.
+        System.out.println();
+        // [9-2] 잘못된 예
+        for (int i = 0; i<atrAr.length;i++)
+            for (int j = 0; j< atrAr.length; j++)
+                System.out.println(atrAr[i].charAt(j)); // hon kim par
+
+        System.out.println(atrAr.length); // 3
+        // System.out.println(atrAr[0].length); // Err
+        System.out.println(atrAr[0].length()); // 4
+
+        for (int i = 0; i< atrAr.length; i++)
+            for (int j = 0; j<atrAr[i].length(); j++)
+                System.out.print(atrAr[i].charAt(j) + " ");
+            System.out.println();
+
+        // [10] : length vs length()
+        int[] aaa = new int[10];
+        System.out.println(aaa.length); // 10
+
+        String bbb = "Welcome to Seoul";
+        System.out.println(bbb.length()); // 16 (공백 문자 포함)
+
         //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
         /*
-        2.
+        11. 사용자 입력을 받아 2차원 배열을 생성하고 값을 입력하는 코드를 구현하시오.
+            이때, 중첩 반복문을 사용하여 출력도 같이 해보시오.
+            이 문제는 배열 생성시 사용자로 부터 입력을 받아서 2차원 배열을 반들 수 있는지를 묻는 문제이다.
+
+            [결과 출력]
+            행의 갯수를 입력하고 [Enter] 치세요 = 3
+            열의 갯수를 입력하고 [Enter] 치세요 = 3
+            1번째 행에 입력할 문자 3개를 차례대로 입력하고 [Enter] 치세요 = #$ㅍ
+            2번째 행에 입력할 문자 3개를 차례대로 입력하고 [Enter] 치세요 = ^&*
+            3번째 행에 입력할 문자 3개를 차례대로 입력하고 [Enter] 치세요 = !@#
+            #$ㅍ
+            ^&*
+            !@#
         * */
+
+        // [1] : 사용자 입력을 위한 객체 선언
+        Scanner sc = new Scanner(System.in);
+
+        // [2] : 2차원 배열의 행과 열 선언 -> 사용자 입력으로 처리
+        System.out.print(" 행의 갯수를 입력하고 [Enter] 치세요 = ");
+        int R = sc.nextInt();
+        System.out.print(" 열의 갯수를 입력하고 [Enter] 치세요 = ");
+        int C = sc.nextInt();
+
+        // [3] : char 2차원 배열 선언 -> 사용자로 부터 R, C 입력받아 -> gameMap[R][C] 생성.
+        char[][] gameMap = new char[R][C];
+
+        // [4] : 사용자 입력을 받아서 저장할 String 배열 선언 -> 이때 크기를 [R]로 해주도록 한다.(중요!)
+        // 크기를 R만큼만 해주면 되는 이유? -> 어차피 사용자가 입력하는 값은 안쪽 for문을 돌면서 2차원 배열에 입력하기 때문이다.
+        String[] strAr10_1 = new String[R];
+
+        // [5] : 2차원 배열 요소 -> 사용자가 입력하는 값으로 세팅
+        for (int i = 0; i<R; i++){
+            System.out.print((i+1)+ "번째 행에 입력할 문자"+ C +"개를 차례대로 입력하고 [Enter] 치세요 =");
+            strAr10_1[i] = sc.next();
+            for (int j = 0; j<C; j++){
+                gameMap[i][j] = strAr10_1[i].charAt(j); // j 인덱스에 있는 값을 하나씩 가져와서 -> gameMap 좌표에 세팅
+            };
+        };
+
+        // [6] : 2차원 배열에 들어있는 값 출력하기
+        System.out.println(strAr10_1);  // 참조 주소값 출력
+        System.out.println(strAr10_1[0]); // 0번째 값
+        System.out.println(strAr10_1[1]); // 1번째 값
+
+        System.out.println("---------------------------------------------------");
+        for (int i = 0; i<R; i++){
+            for (int j = 0; j<C; j++){
+                System.out.print(gameMap[i][j]+ " ");
+            };
+            System.out.println();
+        };
+        System.out.println("---------------------------------------------------");
         //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
         /*
@@ -230,10 +404,6 @@ public class Array {
 
 
     }
-
-
-
-
 
 
 }
